7ea2def2537911687204e304fc3d4d9ffdd06a21
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-28 18:47:40 +0000

    M - 1:47 PM - syntax error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-    last_number = number[-1]
+    last_number = number % 10
 else:
-    last_number = number[-1] * -1
+    last_number = ((number * -1) % 10) * -1
 
 if last_number > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_number))
