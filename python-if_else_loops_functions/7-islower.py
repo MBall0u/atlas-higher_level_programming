c0eec31e6a8fc1c3d7805405a79508d6e7c125a3
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-01 00:36:58 +0000

    M - 7:36 PM - NULL case?

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord('c') >= 97 and ord('c') <= 122):
+    if c is None:
+        return None
+    elif (ord('c') >= 97 and ord('c') <= 122):
         return True
     else:
         return False
