6470fdf478a68c91faf6d03eb5e9e6bafd90eb1b
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-03 20:38:42 +0000

    M - 3:38 PM - made file args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} areguments:".format(len(argv)))
+        for i in range(len(argv))
+            print("{}: {}".format(i, sys.argv[i]))
