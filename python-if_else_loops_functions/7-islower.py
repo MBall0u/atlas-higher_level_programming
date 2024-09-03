10c8b9761b17b0df4e64342096409ebd521e37bb
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-03 12:37:33 +0000

    adfnaksjdn

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def islower(c):
     if c == '':
         return None
-    elif (ord('c') >= 97 and ord('c') <= 122):
+    elif ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
