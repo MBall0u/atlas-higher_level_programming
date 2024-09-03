d7a24f6830999312db4c5bf4fdd27aa83bf227db
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-03 20:41:43 +0000

    M - 3:41 PM - args error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(argv) == 1:
+    if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} areguments:".format(len(argv)))
-        for i in range(len(argv)):
+        print("{} areguments:".format(len(sys.argv)))
+        for i in range(len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
