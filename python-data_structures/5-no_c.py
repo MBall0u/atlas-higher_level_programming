b0fa2e325d9182d4c1707362c12e822487b35315
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-04 01:23:50 +0000

    M - 8:23 PM - klsf

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in (len(my_string)):
-        if ord(my_string[i]) != 67 or ord(my_string[i]) != 99:
-            print("{}".format(my_string[i]), end='')
+    new_string = ""
+    for i in (my_string):
+        if ord(i) == 67 or ord(i) == 99:
+            print("", end='')
+        else:
+            new_string += i
+    return new_string
