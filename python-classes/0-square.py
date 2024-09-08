f941b492cfa133998b257ad2b7d0382191ab4b8b
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-08 22:29:10 +0000

    M - 5:21 PM - trying to figure out documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""
+
+This module contains the square class definition.
+
+Classes:
+    Square: Represents a simple square shape.
+
+"""
+
 class Square:
     """
 
-    Represents a simple square shape
+    Represents a simple square shape.
 
     """
     pass
