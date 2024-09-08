078cafc8590beb78df06d49976d0f2b81f8df9e1
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-08 22:47:24 +0000

    M - 5:47 PM - made the file for 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+
+This module contains the square class definition.
+
+Classes:
+    Square: Represents a simple square shape.
+
+"""
+
+
+class Square:
+    """
+
+    Represents a simple square shape.
+
+    Attributes:
+        size (int): The size of the square.
+
+    """
+
+
+    def __init__(self, size):
+        """
+
+        Initializes a square object.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
+
+    pass
