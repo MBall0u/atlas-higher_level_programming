4102b9fdc678161688989120563c7c7e75fa7515
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-08 22:59:23 +0000

    M - 5:59 PM - made file for 2-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+
+This module contains the square class definition.
+
+Classes:
+    Square: Represents a simple square shape.
+
+"""
+
+
+class Square:
+    """
+
+    Represents a simple square shape.
+
+    Attributes:
+        size (int): The size of the square.
+
+    """
+
+    def __init__(self, size=0):
+        """
+
+        Initializes a square object.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    pass
