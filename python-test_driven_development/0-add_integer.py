153e27742d68722f75d741ad07ec48733f6b0cb7
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-09 15:16:42 +0000

    M - 10:16 AM - made the file add_integer and the test text file for add_integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+
+    Adds two integers together.
+
+    Args:
+        a (int or float): The first number to add. Must be provided.
+        b (int or float): The second number to add. Optional, defaults to 98.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+
+    Returns:
+        int: The sum of a and b as integers.
+
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
