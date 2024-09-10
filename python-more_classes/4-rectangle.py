b28a7b00566168f3f56f2f91c8b1004b8108890f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-10 18:32:31 -0500

    M - 6:30 PM - made file for 4-rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -148,4 +148,6 @@ class Rectangle:
             str: string representation of the rectangle.
         """
 
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        test = "Rectangle({}, {})".format(self.__width, self.__height)
+
+        return test
