d6a27e500fea668e2dd3ccc551b98a7b3158ab0b
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-10 14:04:03 -0500

    M - 2:03 PM - made the new directory, readme, and the file rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+This module contains the rectangle class definition.
+
+Classes:
+    Rectangle: Represents a simple rectangle shape.
+
+"""
+
+
+class Rectangle:
+    """
+
+    Represents a simple rectangle shape.
+
+    """
+
+    pass
