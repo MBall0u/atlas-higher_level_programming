c2fecb322e6a793e29fc28762c1a6058bcfe3b62
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-11 14:44:24 -0500

    M - 2:44 PM - last commit for PLD

---
+++ 9-11-PLD/RPG_characters.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# marvel rpg game
+class Character:
+    # details about heroes and villains
+    # all have their hero/villain name
+    # all have health
+    # all have their real name
+    # all have powers or no powers
+    
+    pass
+
+class Heroes(Character):
+    pass
+
+class Villains(Character):
+    pass
