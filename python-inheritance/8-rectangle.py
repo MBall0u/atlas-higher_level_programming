e2fa1b168ea5e6d1312700b6e2721b13399dc65e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-13 19:31:51 +0000

    M - 2:31 PM - finished 8-rectangle/back from lunch

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,11 +4,18 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
+        vtype = type(value)
+        if vtype is not int:
+            raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError("{} must be greater than 0".format(name))
+
+        return None
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
+        if super().integer_validator("width", width) is None:
+            if super().integer_validator("height", height) is None:
+                self.__width = width
+                self.__height = height
