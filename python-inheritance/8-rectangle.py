e49fbd0e5e650530440bc5063917854c2d34e780
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-13 17:36:17 +0000

    M - 12:36 PM - going to lunch

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(self.name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
