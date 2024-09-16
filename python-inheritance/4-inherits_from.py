200728430ed212609e860b37d6f9b1c75ff0c9de
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-16 17:50:54 -0500

    M - 5:50 PM - made docstrings for 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+
+This is a simple module.
+
+"""
 def inherits_from(obj, a_class):
+    """
+
+    returns True if the object is an instance of a
+    class that inherited (directly or indirectly) from
+    the specified class ; otherwise False.
+
+    Args:
+        obj (object): object.
+        a_class (class): a specified class.
+
+    Returns:
+        bool: True if the object is an instance of a
+        class that inherited (directly or indirectly)
+        from the specified class ; otherwise False.
+    """
+
     obj_type = type(obj)
     if obj_type == a_class:
         return False
