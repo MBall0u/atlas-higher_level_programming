50c72532c5c25d94c3f9cb3771cc7ea8a601fa80
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-16 16:19:36 -0500

    M - 4:17 PM - added docstring for 1-my_list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """
 
-This returns the list of available attributes
-and methods of an object.
+This is a simple module.
 
 """
 
 
 def lookup(obj):
+    """
+
+    This returns the list of available attributes
+    and methods of an object.
+
+    Args:
+        obj (list): _description_
+
+    Returns:
+        _type_: _description_
+    """
     my_list = []
     attrs = dir(obj)
     my_list.append(attrs)
