f999082104e5a3f0440f11cd16d85f3cbb241ce2
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-16 17:45:58 -0500

    M - 5:45 PM - fixed docstring for 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python3
+"""
+
+This is a simple module.
+
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """
+
+    returns True if the object is an instance of, or
+    if the object is an instance of a class that inherited
+    from, the specified class ; otherwise False.
+
+    Args:
+        obj (object): an object.
+        a_class (class): a given class.
+
+    Returns:
+        bool: true if the object is an instance of or
+        an instance of the class that was inherited
+        from the specified class, otherwise false.
+    """
+
     return isinstance(obj, a_class)
