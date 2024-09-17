d2865315e3bb6bc479b6409ce4e7fba18c641c78
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-17 16:25:19 -0500

    M - 4:25 PM - wrote the docstrings for 1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+
+This is a simple module.
+
+"""
+
+
 def write_file(filename="", text=""):
+    """
+
+    This writes to a file.
+
+    Args:
+        filename (str, optional): the filename that is
+        being wrote. Defaults to "".
+        text (str, optional): The input that is being
+        wrote to the file. Defaults to "".
+
+    Returns:
+        int: number of characters.
+    """
+
     with open(filename, 'w', encoding="utf-8") as f:
         characters = f.write(text)
     return characters
