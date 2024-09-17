b92570b610052ed4fef2ce1e564f0492574da5a6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-17 14:40:10 +0000

    M - 9:39 AM - fixed docstring on 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
     """
 
-    
+    This is a simple module.
 
     Raises:
-        Exception: _description_
-        TypeError: _description_
-        ValueError: _description_
+        Exception: area() is not implemented.
+        TypeError: must be an integer.
+        ValueError: must be greater than 0.
 
     Returns:
-        _type_: _description_
+        bool: None
     """
 
 
 class BaseGeometry:
     """
-    
-    
-    
+
+    This is a simple base class.
+
     """
 
     def area(self):
         """
 
-        
+        This is not implemented for the base class.
 
         Raises:
-            Exception: _description_
+            Exception: area() is not implemented.
         """
 
         raise Exception("area() is not implemented")
@@ -34,18 +34,18 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
 
-        
+        This type checks and sees if the value is greater than 0.
 
         Args:
-            name (_type_): _description_
-            value (_type_): _description_
+            name (str): name of the variable.
+            value (int): value of the given variable.
 
         Raises:
-            TypeError: _description_
-            ValueError: _description_
+            TypeError: must be an integer.
+            ValueError: must be greater than 0.
 
         Returns:
-            _type_: _description_
+            bool: None
         """
 
         vtype = type(value)
@@ -60,20 +60,20 @@ class BaseGeometry:
 class Rectangle(BaseGeometry):
     """
 
-
+    This is a derived class from BaseGeometry.
 
     Args:
-        BaseGeometry (_type_): _description_
+        BaseGeometry (class): parent class.
     """
 
     def __init__(self, width, height):
         """
 
-
+        This initializes the width an height in the parent class.
 
         Args:
-            width (_type_): _description_
-            height (_type_): _description_
+            width (int): provided width.
+            height (int): provided height.
         """
 
         if super().integer_validator("width", width) is None:
