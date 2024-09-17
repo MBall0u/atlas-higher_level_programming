e46d342783b1686363f47b2b4614e9b0926b89e0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-17 16:36:21 -0500

    M - 4:35 PM - made docstrings for 2-append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+"""
+
+This is a simple module.
+
+"""
+
+
 def append_write(filename="", text=""):
+    """
+
+    appends text to a file.
+
+    Args:
+        filename (str, optional): the given file that the text
+        is being appended to. Defaults to "".
+        text (str, optional): the text that is being append to
+        the file. Defaults to "".
+
+    Returns:
+        int: the number of characters written.
+    """
     with open(filename, 'a', encoding="utf-8") as f:
         characters = f.write(text)
     return characters
