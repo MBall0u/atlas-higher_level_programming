ed11057706be1e38057dc8fd2d53cf947432d51c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-10-15 20:54:12 -0500

    M - 8:46 PM - made the file 7-cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- this is a comment
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
