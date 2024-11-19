64656974e3419d4b0d9616b555e8d282207c374e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-19 20:55:50 +0000

    M

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Square = require('5-square.js');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
+class Square extends Square {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.width));
     }
