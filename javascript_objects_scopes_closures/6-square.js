e2e7effeffa7ea9d0780475cad2acdc6407f2fd6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-19 21:00:35 +0000

    M - 3:00 PM - had to change the imports name

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Square = require('5-square.js');
+const squareV1 = require('5-square.js');
 
-class Square extends Square {
+class Square extends squareV1 {
   charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.width));
