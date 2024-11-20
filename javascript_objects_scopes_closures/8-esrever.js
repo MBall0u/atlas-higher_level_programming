65be8178d266327b8d9095d5a800ca059faf82e6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-20 15:43:16 +0000

    M - 9:42 AM - mad the file 8-esrever

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 exports.esrever = function (list) {
   for (let i = 0; i < (list.length / 2); i++) {
     const temp = list[i];
-    list[i] = list[list.length - i];
-    list[list.length - i] = temp;
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = temp;
   }
   return list;
 };
